68a1db28bffc45976661c491d662285551b4cf25
Dylan Musil <dmmusil@ncsu.edu>
2022-05-13 22:16:59 -0400

    Display username after login

--- test/pento_web/controllers/user_registration_controller_test.exs
+++ test/pento_web/controllers/user_registration_controller_test.exs
@@ -22,10 +22,11 @@ defmodule PentoWeb.UserRegistrationControllerTest do
     @tag :capture_log
     test "creates account and logs the user in", %{conn: conn} do
       email = unique_user_email()
+      user = valid_user_attributes(email: email)
 
       conn =
         post(conn, Routes.user_registration_path(conn, :create), %{
-          "user" => valid_user_attributes(email: email)
+          "user" => user
         })
 
       assert get_session(conn, :user_token)
@@ -34,7 +35,7 @@ defmodule PentoWeb.UserRegistrationControllerTest do
       # Now do a logged in request and assert on the menu
       conn = get(conn, "/")
       response = html_response(conn, 200)
-      assert response =~ email
+      assert response =~ user.username
       assert response =~ "Settings</a>"
       assert response =~ "Log out</a>"
     end
